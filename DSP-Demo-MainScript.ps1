613d3f862f4525a2e09431608052687ef2f87635
Bob Lyons <bob.lyons.202@gmail.com>
2025-11-20 10:45:07 -0500

    fix module names

--- DSP-Demo-MainScript.ps1
+++ DSP-Demo-MainScript.ps1
@@ -243,9 +243,9 @@ function Main {
         "DSP-Demo-02-Setup-CreateGroups",
         "DSP-Demo-03-Setup-CreateUsers",
         "DSP-Demo-04-Setup-CreateComputers",
-        "DSP-Demo-05-Setup-CreateDefaultDom",
-        "DSP-Demo-06-Setup-CreateGPP",
-        "DSP-Demo-07-Setup-CreateADSitesAnds",
+        "DSP-Demo-05-Setup-CreateDefaultDomainPolicy",
+        "DSP-Demo-06-Setup-CreateFGPP",
+        "DSP-Demo-07-Setup-CreateADSitesAndsAubnets",
         "DSP-Demo-08-Setup-CreateDNSZones",
         "DSP-Demo-09-Setup-CreateGPOs"
     )
